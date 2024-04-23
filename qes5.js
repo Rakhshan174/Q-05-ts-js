321c43a1a68a64d55431a584cb969d4e656f0eea
Rakhshan174 <160410026+Rakhshan174@users.noreply.github.com>
2024-04-23 05:26:38 -0700

    q 05 TS/JS for 45 assignment

---
+++ qes5.js
@@ -0,0 +1,8 @@
+/*
+Repeat Exercise 4, but this time store the famouse person's name in a variable called
+famouse_person. Then compose your message and store it in a new variable called message. Print
+your message.
+*/
+var famousPerson = " Albert Einstein";
+var message = "".concat(famousPerson, " once said,\" A Person who never tried anything new.\"");
+console.log(message);
---
+++ qes5.ts
@@ -0,0 +1,9 @@
+/* 
+Repeat Exercise 4, but this time store the famouse person's name in a variable called
+famouse_person. Then compose your message and store it in a new variable called message. Print
+your message.
+*/
+const famousPerson: String = " Albert Einstein";
+
+const message: string = `${famousPerson} once said," A Person who never tried anything new."`;
+console.log(message);
